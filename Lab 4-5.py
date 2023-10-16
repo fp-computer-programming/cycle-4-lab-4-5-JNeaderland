a2332585b3172c2e41a8debdc4f0a83b46bdbb80
JNeaderland <144148081+JNeaderland@users.noreply.github.com>
2023-10-16 14:36:47 -0400

    Add files via upload

---
+++ Lab 4-5.py
@@ -0,0 +1,5 @@
+#AUthor: Jacob Neaderland
+person1 = input("What is your name? ")
+person2 = input("And what is your name? ")
+sent = "Hello, {0}. My name is {1}.".format(person1, person2)
+print (sent)
